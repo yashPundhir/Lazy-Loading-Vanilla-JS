e6ad99e70355f511472d7842c30f0f4f98fd0970
yashPundhir <yash.pundhir.prof@gmail.com>
2023-09-14 23:38:28 +0530

    function to fetch API data via lazy loading is implemented

--- script.js
+++ script.js
@@ -58,40 +58,113 @@ function createCard(data) {
 // Select the container div
 let container = document.getElementById("container");
 
-async function fetchData() {
-	try {
-		const response = await fetch("https://fakestoreapi.com/products?limit=12");
+const loader = document.getElementById("loader");
 
-		if (!response.ok) {
-			throw new Error("Network response was not ok");
-		}
+const end = document.getElementById("end");
 
-		const data = await response.json();
-
-		return data;
-	} catch (error) {
-		console.error("Fetch error:", error);
-	}
+function showLoadingIndicator() {
+	loader.style.display = "flex";
+}
+function showEndingIndicator() {
+	end.style.display = "flex";
 }
 
-async function main() {
-	try {
-		let products = await fetchData();
-		let data = [];
-		data.push(...products);
-		data.push(...products);
-		data.push(...products);
-		data.push(...products);
-		data.push(...products);
-		// console.log(data[0]);
-		data.forEach((item) => {
-			setTimeout(() => {
-				createCard(item);
-			}, 500);
+function hideLoadingIndicator() {
+	loader.style.display = "none";
+}
+
+const productsPerPage = 12; // Number of products to load per batch
+let isLoading = false; // Flag to prevent multiple simultaneous requests
+let totalProductsLoaded = 0; // Track the total number of products loaded
+
+function loadMoreProducts() {
+	if (isLoading || totalProductsLoaded >= 120) return;
+
+	isLoading = true;
+
+	showLoadingIndicator(); // Show the loading indicator
+
+	// Fetch the next batch of products
+
+	fetch(`https://fakestoreapi.com/products?limit=${productsPerPage}`)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return response.json();
+		})
+		.then((newProducts) => {
+			isLoading = false;
+			hideLoadingIndicator(); // Hide the loading indicator
+			if (newProducts.length === 0) {
+				// No more products to load
+				// You can hide a "Load More" button or provide some feedback
+			} else {
+				// Append the new products to your existing list
+				newProducts.forEach((item) => {
+					createCard(item);
+					totalProductsLoaded++;
+					if (totalProductsLoaded >= 120) {
+						// Stop loading new products when the limit is reached
+						showEndingIndicator();
+						return;
+					}
+				});
+			}
+		})
+		.catch((error) => {
+			console.error("Error loading products:", error);
+			isLoading = false;
 		});
-	} catch (error) {
-		console.error("Main error:", error);
-	}
 }
 
-main();
+// Event listener for scroll
+window.addEventListener("scroll", () => {
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+	// Check if the user has scrolled near the bottom
+	if (scrollTop + clientHeight >= scrollHeight - 200) {
+		loadMoreProducts();
+	}
+});
+
+// Initial load
+loadMoreProducts();
+
+// async function fetchData() {
+// 	try {
+// 		const response = await fetch("https://fakestoreapi.com/products?limit=12");
+
+// 		if (!response.ok) {
+// 			throw new Error("Network response was not ok");
+// 		}
+
+// 		const data = await response.json();
+
+// 		return data;
+// 	} catch (error) {
+// 		console.error("Fetch error:", error);
+// 	}
+// }
+
+// async function main() {
+// 	try {
+// 		let products = await fetchData();
+// 		let data = [];
+// 		data.push(...products);
+// 		data.push(...products);
+// 		data.push(...products);
+// 		data.push(...products);
+// 		data.push(...products);
+// 		// console.log(data[0]);
+// 		data.forEach((item) => {
+// 			setTimeout(() => {
+// 				createCard(item);
+// 			}, 500);
+// 		});
+// 	} catch (error) {
+// 		console.error("Main error:", error);
+// 	}
+// }
+
+// main();
